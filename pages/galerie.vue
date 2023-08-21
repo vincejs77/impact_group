6979dc013e00f9db41f9c82e8443972a764d372a
vincejs77 <vinjobs77@gmail.com>
2023-08-21 13:00:03 +0200

    update(10-08-2023): 006

--- pages/galerie.vue
+++ pages/galerie.vue
@@ -16,6 +16,16 @@
         </div>
         <div class="w-full mt-8 max-w-2xl mx-auto">
           <ul class="flex justify-center flex-wrap gap-4">
+            <li
+              :class="[
+                currentCategorieID == null
+                  ? 'bg-blue text-white'
+                  : 'bg-gray-200 text-blue',
+                'cursor-pointer  rounded-md px-4 font-medium py-1 text-sm',
+              ]"
+            >
+              <button @click="resetGalerie_fx(null)" type="button">Toutes</button>
+            </li>
             <li :key="categorie" v-for="categorie in data_categories_gallerie">
               <button
                 type="button"
@@ -36,7 +46,30 @@
     </section>
     <section class="i-wrapper">
       <div class="i-container pb-16">
-        <div class="columns-1 md:columns-3 lg:columns-3">
+        <div v-if="data_galerie.length == 0 && !pending_galerie" class="text-center">
+          <div class="flex justify-center items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.182 16.318A4.486 4.486 0 0012.016 15a4.486 4.486 0 00-3.198 1.318M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
+              />
+            </svg>
+          </div>
+          <div class="mt-4 max-w-sm mx-auto">
+            <p class="text-gray-500">
+              Aucune image disponible pour la catégorie sélectionnée pour le moment !
+            </p>
+          </div>
+        </div>
+        <div v-if="!pending_galerie" class="columns-1 md:columns-3 lg:columns-3">
           <div
             :key="image"
             v-for="(image, index) in data_galerie"
@@ -74,6 +107,20 @@
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+
+        <div v-else class="columns-1 md:columns-3 lg:columns-3">
+          <div
+            :key="i"
+            v-for="i in 3"
+            class="mb-4 relative animate-pulse w-full min-h-[200px] bg-blue bg-opacity-10 rounded-lg overflow-hidden group cursor-pointer"
+          >
+            <div class="rounded-md h-5 absolute left-4 right-4 top-4">
+              <div class="w-1/3 bg-white h-5 rounded-md"></div>
+            </div>
+
+            <div class="rounded-md h-5 absolute left-4 right-4 bottom-4 bg-white"></div>
           </div>
         </div>
       </div>
@@ -130,7 +177,14 @@ watch(data_galerie.value, (newDataGalerie) => {});
 
 useGlobalStore().$state.lengthData_galerie = data_galerie.value.length;
 
-const currentCategorieID = ref("");
+const currentCategorieID = ref(null);
+
+const resetGalerie_fx = async (_currentCategorieID) => {
+  query.value = groq`*[_type == "galerie"]| order(_createdAt desc){_id,titre,description,_updatedAt,_createdAt,"imageUrl":image.asset->,categories_gallerie[]->{titre}}`;
+  await refresh_galerie();
+  currentCategorieID.value = _currentCategorieID;
+  useGlobalStore().$state.lengthData_galerie = data_galerie.value.length;
+};
 
 const refreshGalerie_fx = async (_currentCategorieID) => {
   query.value = groq`*[_type == "galerie" && categories_gallerie[]._ref == "${_currentCategorieID}" ]| order(_createdAt desc){_id,titre,description,_updatedAt,_createdAt,"imageUrl":image.asset->,categories_gallerie[]->{titre,_id}}`;
